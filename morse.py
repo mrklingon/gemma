39f5182feebcec6a6b4036cf8a3fbb028cc59d16
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2023-05-15 20:20:50 -0500

    Create morse.py
    morse code translator for gemma

---
+++ morse.py
@@ -0,0 +1,39 @@
+## morse code module
+import time
+
+
+fc = (6,8,1)
+dc = (0,0,40)
+dtc= (0,40,0)
+blank = (0,0,0)
+
+# Dictionary representing the morse code chart
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+   'C':'-.-.', 'D':'-..', 'E':'.',
+   'F':'..-.', 'G':'--.', 'H':'....',
+   'I':'..', 'J':'.---', 'K':'-.-',
+   'L':'.-..', 'M':'--', 'N':'-.',
+   'O':'---', 'P':'.--.', 'Q':'--.-',
+   'R':'.-.', 'S':'...', 'T':'-',
+   'U':'..-', 'V':'...-', 'W':'.--',
+   'X':'-..-', 'Y':'-.--', 'Z':'--..',
+   '1':'.----', '2':'..---', '3':'...--',
+   '4':'....-', '5':'.....', '6':'-....',
+   '7':'--...', '8':'---..', '9':'----.',
+   '0':'-----', ',':'--..--', '.':'.-.-.-',
+   '?':'..--..', '/':'-..-.', '-':'-....-',
+   '(':'-.--.', ')':'-.--.-'
+}
+def encryption(message):
+   my_cipher = ''
+   for myletter in message.upper():
+      try:
+          if myletter != ' ':
+             my_cipher += MORSE_CODE_DICT[myletter] + '  '
+          else:
+             my_cipher += ' '
+      except:
+          my_cipher += ' '
+          
+   return my_cipher
+
